59a84f4ddb3179709afbababcfc124e5fe18be85
Ferdinand Malcher <ferdinand@malcher.media>
2025-09-05 10:30:36 +0200

    playground

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,49 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, effect, resource, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('resource-playground');
+  protected params = signal<string | undefined>('first params');
+
+  protected myResource = resource({
+    params: () => this.params(),
+    loader: ({ params }) => new Promise<string>((resolve, reject) => setTimeout(() => {
+      if (params === 'error') {
+        return reject(new Error('ERROR! ' + Date.now()));
+      }
+      return resolve(`${params} ${Date.now()}`);
+    }, 1500)),
+    defaultValue: 'DEFAULT'
+  });
+
+  constructor() {
+    effect(() => {
+      console.log('### PARAMS', this.params());
+    });
+
+    effect(() => {
+      console.log('status', this.myResource.status());
+    });
+
+    effect(() => {
+      console.log('isLoading', this.myResource.isLoading());
+    });
+
+    effect(() => {
+      console.log('hasValue', this.myResource.hasValue());
+    });
+
+    effect(() => {
+      console.log('value', this.myResource.value());
+    });
+
+    effect(() => {
+      console.log('error', this.myResource.error());
+    });
+
+  }
 }
